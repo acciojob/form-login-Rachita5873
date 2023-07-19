15caf32ced244cb3183c8c8c5e5d00877b11bbcb
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-07-19 22:37:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+	let val1 = document.getElementsByTagName("input")[0];
+	let val2 = document.getElementsByTagName("input")[1];
+	
+    alert(val1.value + " " + val2.value)
 
 }
